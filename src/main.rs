d7f59b281e129f8b1febfb7462905b5ffaf51b3b
Tally <138633627+TallyIsCool@users.noreply.github.com>
2023-07-08 10:37:02 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,24 @@ use std::time::Duration;
 use toy_arms::VirtualKeyCode;
 
 fn main() {
-  println!("When an application is selected, press insert to toggle the mouse fix.");
+  println!("press INS to toggle");
   unsafe {
     loop {
       if toy_arms::detect_keypress(VirtualKeyCode::VK_INSERT) {
         let hwnd: HWND = GetForegroundWindow();
+        SetForegroundWindow(hwnd);
         let mut rect: RECT = std::mem::zeroed();
         GetWindowRect(hwnd, &mut rect);
-        let mut isenabled:bool = false;
         let center_x = (rect.left + rect.right) / 2;
         let center_y = (rect.top + rect.bottom) / 2;
-        SetForegroundWindow(hwnd);
-        isenabled = !isenabled;
-        print!("\x1B[2J\x1B[1;1H");
-        println!("enabled");
-        while isenabled == true {
-        thread::sleep(Duration::from_millis(10));
-        SetCursorPos(center_x, center_y);
+        loop {
+          thread::sleep(Duration::from_millis(50));
+          SetCursorPos(center_x, center_y);   
           if toy_arms::detect_keypress(VirtualKeyCode::VK_INSERT) {
-            print!("\x1B[2J\x1B[1;1H");
-            println!("disabled");
             break;
-          }   
+          }
         }
-      }
+      }   
     }
   }
 }
